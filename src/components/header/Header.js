eb007fbab7fcb61852618d3322c97f2661bc1412
sourav-taleda-code <sourav.taleda03@zohomail.in>
2026-07-20 10:26:16 +0530

    Fix ESLint unused vars in Header.js to fix Vercel build

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,22 +7,18 @@ import {
   greeting,
   workExperiences,
   skillsSection,
-  openSource,
   blogSection,
   talkSection,
-  achievementSection,
-  resumeSection
+  achievementSection
 } from "../../portfolio";
 
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
-  const viewOpenSource = openSource.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
   const viewBlog = blogSection.display;
   const viewTalks = talkSection.display;
-  const viewResume = resumeSection.display;
 
   return (
     <Headroom>
